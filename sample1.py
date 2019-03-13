e93264848ff9c472606ce30fef95fcf39af50a1c
sbwtk6122001 <matsumoto@consulnet.jp>
2019-03-13 20:24:24 +0900

    Update sample1.py

--- sample1.py
+++ sample1.py
@@ -24,7 +24,7 @@ while True:
          if data.tick_last_update > latest_update:
             print('Illumination: {} lx'.format(data.val_light))
             print('temp:{}'.format(data.val_temp))
-            print('sound:{}db / humidty:{}'.format(data.val_noise,data.val_humi))
+            print('sound:{}db / humidty:{}%'.format(data.val_noise,data.val_humi))
             print('{}lx /{}℃'.format(data.val_light, data.val_temp))
 
         latest_update = data.tick_last_update
